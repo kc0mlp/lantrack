264404748f006e0acfc09747bdeee59c0dcd3ab9
Kael Hankins <kc0mlp@gmail.com>
2013-11-14 00:13:19 -0600

    Fixed ruby 2.0.0 incompatibilities. Readded Future Version Ideas and data-model to docs.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Lantrack::Application.config.session_store :cookie_store, key: '_lantrack_session'
+Lantrack::Application.config.session_store :cookie_store, :key => '_lantrack_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
