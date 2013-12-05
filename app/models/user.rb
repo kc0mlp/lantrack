03260694c60346db188d1cdf666de6c846b9c2dd
unknown <kc0mlp@.(none)>
2013-12-04 20:49:03 -0600

    -Trying to understand why belongs_to/has_many associations do not seem to work quite correctly.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-  belongs_to :role
+  belongs_to :role, :class_name => 'Role'
   attr_accessible :created_by, :first_name, :crypt_hash, :last_name, :role_id, :username
 end
